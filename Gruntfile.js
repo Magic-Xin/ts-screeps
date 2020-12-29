4f8f572306964b8ce8f071db97dbf8e5ce32f4c7
Magic-Xin <mail@magicxin.tech>
2020-12-29 23:54:11 +0800

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function (grunt) {
     grunt.loadNpmTasks("grunt-ts");
     grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-copy");
 
     grunt.initConfig({
         'ts': {
@@ -17,12 +18,20 @@ module.exports = function (grunt) {
         },
         uglify: {
             default: {
-                files: [{
-                   'dist/all.js':['dist/*.js'],
-                }]
-             }
+                files: {
+                    'dist/min/main.js': ['dist/*.js'],
+                }
+            }
+        },
+        copy: {
+            default: {
+                expand: true,
+                cwd: 'dist/min/',
+                src: 'main.js',
+                dest: 'out/'
+            }
         }
     })
     // 将 ts 编译任务注册到默认执行命令
-    grunt.registerTask('default', ["ts" , "uglify"])
+    grunt.registerTask('default', ["ts", "uglify", "copy"])
 }
--- dist/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var clean_1 = require("./clean");
-module.exports.loop = function () {
-    clean_1.default.run();
-};
